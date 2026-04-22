032cfb285d42aec3cc0fd0f521487da569a2e479
Kavish Pal Singh <23f2005144@ds.study.iitm.ac.in>
2026-04-23 00:07:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from application.database import db
 app=Flask(__name__,template_folder= 'templates',static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///household_serv_database.sqlite3"
 db.init_app(app)
-app.app_context().push()
+
 
 
 from application.controllers import*
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run()
 
 
 
